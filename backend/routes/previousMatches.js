83157b5cb0cba58b55d01f730b32c4a4d9d21fa3
Suraj jain <suraj.2125cs1087@kiet.edu>
2024-04-25 02:55:01 +0530

    Issues fixed

--- backend/routes/previousMatches.js
+++ backend/routes/previousMatches.js
@@ -6,7 +6,7 @@ const Match = require('../model/match');
 router.get('/', async (req, res) => {
   try {
     const today = new Date();
-    const previousMatches = await Match.find({ matchDate: { $lt: today } });
+    const previousMatches = await Match.find({ matchDate: { $lt: today } }).sort({ matchDate: -1 });
     res.send(previousMatches);
   } catch (err) {
     res.status(500).json({ message: err.message });
